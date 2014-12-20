9b5997d7bc07ba98fe002d97c75f8a7920ec7085
Polankiewicz <damianpol2@o2.pl>
2014-12-20 15:58:34 +0100

    Prototyp menu głównego

---
+++ SpaceZombieShooter/Assets/Scripts/LightUpElement.js
@@ -0,0 +1,21 @@
+﻿renderer.material.color = Color.grey;
+
+function OnMouseEnter () 
+{
+	renderer.material.color = Color.white;
+}
+
+function OnMouseExit () 
+{
+	renderer.material.color = Color.grey;
+}
+
+function Update()
+{
+	if(Input.GetKey(KeyCode.Escape)) Application.Quit();
+}
+
+//function OnMouseUp()
+//{
+//	if()
+//}
